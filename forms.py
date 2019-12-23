6825803153044553cf422e50646663aa4567104f
xoak <54974852+xoak@users.noreply.github.com>
2019-12-22 23:27:32 -0800

    error handling/check invalid inputs

--- forms.py
+++ forms.py
@@ -1,19 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Length, NumberRange
 
 class RegisterForm(FlaskForm):
     user = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=15)])
-    zipcode = StringField('Zip Code', validators=[DataRequired(), Length(min=5, max=5)])
+    zipcode = IntegerField('Zip Code', validators=[DataRequired(), NumberRange(min=10000, max=99999, message = "Must be exactly 5 Digits.")])
     submit = SubmitField('Register')
-
-    #def validate_user(self, user):
-    #    user_count = User.query.filter_by(username=user.data).count()
-    #    if user_count > 0:
-    #        raise ValidationError('User already exists')
 
 class LoginForm(FlaskForm):
     user = StringField('Username', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
--- weather.py
+++ weather.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, redirect, flash, url_for
-
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegisterForm, LoginForm
 from datetime import datetime
@@ -10,35 +9,34 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2938hacvw98y213rn'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True, nullable=False)
-    password = db.Column(db.String(15), unique=True, nullable=False)
+    password = db.Column(db.String(15), unique=False, nullable=False)
     zipcode = db.Column(db.String(5), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @app.route('/')
 def display_weather():
     if current_user.is_authenticated:
+        date = datetime.now().strftime("%B %d, %Y @ %-I:%M%p")
         r = requests.get('https://api.openweathermap.org/data/2.5/weather?zip=' + current_user.zipcode + ',us&units=imperial&appid=118e5c233596829cee385d6d18f89d46').json()
         weather_data = {
             "city" : r['name'],
-            "temp" : r['main']['temp'],
+            "temp" : int(r['main']['temp']),
             "humidity" : r['main']['humidity'],
             "description" : r['weather'][0]['description']
         }
-        return render_template('weather.html', title='Weather', weather_data = weather_data)
+        return render_template('weather.html', title='Weather', weather_data = weather_data, date=date)
     else:
         return render_template('weather.html', title='Weather')
 
@@ -49,7 +47,7 @@ def register():
         user = User(username=form.user.data, password=form.password.data, zipcode=form.zipcode.data)
         db.session.add(user)
         db.session.commit()
-        flash('account created successfully')
+        flash('Account Created Successfully', 'success')
         return redirect('/login')
     return render_template('register.html', title='Register', form=form)
 
@@ -64,7 +62,7 @@ def login():
             login_user(user)
             return redirect('/')
         else:    
-            flash('login failed')
+            flash('Login Failed', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 @app.route('/logout')
